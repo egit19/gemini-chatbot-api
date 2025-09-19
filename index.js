7acd53247b90e05915dbbf9ed7f695dfb430a4f5
Egit Heri Wicaksono <egitheriwicaksono@gmail.com>
2025-09-19 22:55:48 +0800

    add endpoint chat and create ui public

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const ai = new GoogleGenAI({});
 // inisialisasi model ai
 const geminiModels = {
     text: "gemini-2.5-flash-lite",
+    chat: "gemini-2.5-pro",
     image: "gemini-2.5-flash",
     audio: "gemini-2.5-flash",
     document: "gemini-2.5-flash-lite",
@@ -25,6 +26,8 @@ const geminiModels = {
 app.use(cors());
 
 app.use(express.json());
+
+app.use(express.static('static'));
 
 // inisialisasi route
 app.post('/generate-text',async (req, res) => {
@@ -43,6 +46,72 @@ app.post('/generate-text',async (req, res) => {
     });
 
     res.status(200).json({reply: response.text});
+});
+
+app.post('/chat',async (req, res) => {
+    const { conversation } = req.body;
+
+    if(!conversation || !Array.isArray(conversation)) {
+        res.status(400).json({
+            success: false,
+            data: null,
+            message: "Perakapan tidak valid"
+        });
+    }
+
+    let dataIsInvalid = false;
+
+    [].forEach(item => {
+        if(!item) {
+            dataIsInvalid = true;
+            return;
+        } else if(typeof item !== 'object') {
+            dataIsInvalid = true;
+            return;
+        } else if(!item.role || !item.message) {
+            dataIsInvalid = true;
+            return;
+        }
+    });
+
+    if(dataIsInvalid){
+        return res.status(400).json({
+            success: false,
+            data: null,
+            message: "Ada data yang invalid pada percakapan yang di kirim."
+        });
+    }
+
+    // maping
+    const contents = conversation.map(item => {
+        return {
+            role: item.role,
+            parts: [
+                {text: item.message}
+            ]
+        };
+    });
+
+    try {
+        const aiResponse = await ai.models.generateContent({
+            model: geminiModels.chat,
+            contents,
+        });
+
+        return res.status(200).json({
+            success: true,
+            data: aiResponse.text,
+            message: null
+        });
+    } catch (e) {
+        console.log(e.code);
+        return res.status(500).json({
+            success: false,
+            data: null,
+            message: e.message || "Terjadi kesalahan pada server"
+        })
+    }
+
 });
 
 
